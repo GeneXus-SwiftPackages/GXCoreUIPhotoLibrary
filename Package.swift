7c1f8c4346461e13d0cff7d164243b3cb0734763
Build Er <builder@macmini6.local>
2023-05-14 23:40:53 -0300

    Package version: 1.0.0-beta+20230515023341

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreUIPhotoLibrary",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUIPhotoLibrary-1.0.0-beta+20230515020747.xcframework.zip",
-			checksum: "65746ae83c9ded9d23c51d9469fb7d3b4706cd262b37e60eea0711b54cdbcebe"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUIPhotoLibrary-1.0.0-beta+20230515023341.xcframework.zip",
+			checksum: "61adb8f7cc3e4fb77b4ff02032e0e6f850190b262ac615e12b19856a96b63ed3"
 		)
 	]
 )
