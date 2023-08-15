fd5c338cac4c9dbac43433810ba3c2ab6c929ba0
genexusbot <builder@genexus.com>
2023-08-15 10:24:00 -0300

    Package version: 1.1.0

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreUIPhotoLibrary",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreUIPhotoLibrary",
+			targets: ["GXCoreUIPhotoLibraryWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+	],
+	targets: [
+		.target(name: "GXCoreUIPhotoLibraryWrapper",
+				dependencies: [
+					"GXCoreUIPhotoLibrary",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreUIPhotoLibrary",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreUIPhotoLibrary-1.1.0.xcframework.zip",
+			checksum: "8ef1fc04166d77117d9b8924d8774a0de1f25495824fc66b20dc856675cfe010"
+		)
+	]
+)
