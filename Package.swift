2efcfdc1a491374a561921624971f65f983594f4
Build Er <builder@macmini6.local>
2024-05-06 10:07:22 -0300

    Package version: 2.0.0-beta.25

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreUIPhotoLibraryWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.24")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.25")
 	],
 	targets: [
 		.target(name: "GXCoreUIPhotoLibraryWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreUIPhotoLibrary",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUIPhotoLibrary-2.0.0-beta.24.xcframework.zip",
-			checksum: "bd0848f95bdd5f73bcf65537fd5f8bed645852f5955ed15b90ee04a1d4f7af46"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreUIPhotoLibrary-2.0.0-beta.25.xcframework.zip",
+			checksum: "31fe53ba7088af49ace3c5d23580f88cc06b5b76d2ca85bbe11508c319abb05e"
 		)
 	]
 )
